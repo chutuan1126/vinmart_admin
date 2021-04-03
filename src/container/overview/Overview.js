bd6f207b200b790a1b99f7f1bc4023bb76a05cf8
ChuTuan1126 <chutuan1126@gmail.com>
2021-04-04 02:32:17 +0700

    tuancv: update router

---
+++ src/container/overview/Overview.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+// import { Page } from 'components';
+// import { Header, Statistics, Notifications, Projects, Todos } from './components';
+// const useStyles = makeStyles(styles);
+
+function Overview() {
+  // const classes = useStyles();
+
+  return (
+    <div>Overview</div>
+    // <Page className={classes.root} title="Overview">
+    //   <Header />
+    //   <Statistics className={classes.statistics} />
+    //   <Notifications className={classes.notifications} />
+    //   <Projects className={classes.projects} />
+    //   <Todos className={classes.todos} />
+    // </Page>
+  );
+};
+
+export default Overview;
